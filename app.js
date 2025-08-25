f2d8bd9068ad231cf3424342e38ae8e2a177c839
Spandan More <spandanmore192003@gmail.com>
2025-08-25 10:32:58 +0530

    starting server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
