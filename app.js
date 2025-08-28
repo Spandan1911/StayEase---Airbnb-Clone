e8d9fe188a75984727ad9b07792d7f1b1a4bcf9d
Spandan More <spandanmore192003@gmail.com>
2025-08-28 15:15:16 +0530

    Update app.js
    Add Mongo DataBase

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 
 app.get("/", (req, res) => {
