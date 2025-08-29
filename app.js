9ae111a671af399ecaea832bffc30613bfdfb14a
Spandan More <spandanmore192003@gmail.com>
2025-08-29 22:12:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const mongoose = require("mongoose");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
