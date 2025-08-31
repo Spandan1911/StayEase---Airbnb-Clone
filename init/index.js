d9bf0d2e3b8b589d61224e009c563473ee104f3a
Spandan More <spandanmore192003@gmail.com>
2025-08-31 23:06:53 +0530

    Update index.js
    Connecting Database

--- init/index.js
+++ init/index.js
@@ -3,3 +3,11 @@ const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
